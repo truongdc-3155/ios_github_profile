ea6c3496f5eff40c8a8faa0946a3fa8f89760858
truongdc-3155 <dang.chi.truong@sun-asterisk.com>
2026-05-10 17:10:22 +0700

    Initial Commit

---
+++ github_profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github_profile
+//
+//  Created by dang.chi.truong on 10/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
